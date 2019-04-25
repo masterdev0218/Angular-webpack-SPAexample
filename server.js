bfee38ec89c1f5cf8eb36b3d4ace6b6fdf363d15
Daniel Lee <daniel.lee0218929@gmail.com>
2019-04-25 20:02:21 +0200

    readme updated, code commented

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.set('views', 'dist');
 
+// use express to serve static file 'dist' (index set to false to disable directory indexing to default 'index.html')
 app.use('/', express.static('dist', { index: false }));
 
+// retrieve all (*) data from the server to render on the index page
 app.get('/*', (req, res) => {
    res.render('./index', {req, res});
 });
 
+// app to listen for requests on port defined already
 app.listen(port, () => {
    console.log(`Listening on: http://localhost:${port}`);
 });
